6a2efe358c9425d6a28af73a9dc1b5aa7862055c
AlexXul <110306856+AlexXul@users.noreply.github.com>
2024-04-16 18:48:58 -0600

    Subiendo el archivo java

---
+++ Clase.java
@@ -0,0 +1,14 @@
+public class Clase{
+    private String titulo;
+    private String nombreExpo;
+    private int duracion;
+    public Clase(){
+        
+    }
+}public class Clase{
+    private int id;
+    private String codigoEvento;
+    private double costoIngreso;
+    public Clase(){
+    }
+}
