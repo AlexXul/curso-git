5bab075b47c02218a38699a3e1fbc29d9fbd9257
AlexXul <110306856+AlexXul@users.noreply.github.com>
2024-04-30 18:19:24 -0600

    poniendo nombre de clase Clase.java

--- Clase.java
+++ Clase.java
@@ -2,6 +2,8 @@ public class Clase{
     private String titulo;
     private String nombreExpo;
     private int duracion;
+    private String nombre;
+    private String apellido;
     public Clase(){
         
     }
