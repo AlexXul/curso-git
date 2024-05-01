8e03885034f73882c3feccd0e0b9f66ee1ff4fd6
AlexXul <110306856+AlexXul@users.noreply.github.com>
2024-04-30 18:35:34 -0600

    Update Clase.java

--- Clase.java
+++ Clase.java
@@ -1,9 +1,10 @@
 public class Clase{
-    private String titulo;
+    private String TITULO;
     private String nombreExpo;
     private int duracion;
     private String nombre;
     private String apellido;
+    private String email;
     public Clase(){
         
     }
